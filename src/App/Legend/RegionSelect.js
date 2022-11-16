24503618f5423c1f0c2873c6ea5aa2393121e587
Chris Hull <chris.f.hull@gmail.com>
2022-11-16 17:09:44 +0000

    keep prev state for region selection

--- src/App/Legend/RegionSelect.js
+++ src/App/Legend/RegionSelect.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRegionConfig, useDashboardStore } from '@hyperobjekt/react-dashboard';
 import { useMapState } from '@hyperobjekt/mapgl';
 import { useLocationStore } from '@hyperobjekt/react-dashboard';
@@ -16,14 +16,24 @@ const RegionSelect = (props) => {
   const setRegion = useDashboardStore((state) => state.setRegion);
   const currentRegion = regions.find((r) => r.id === region);
   const map = useMapState('map');
+  const selected = useLocationStore((state) => state.selected);
+
+  // Stores the selected cities/states so toggling regions back and forth will keep previous state
+  const [cachedSelection, setCachedSelection] = useState({ cities: [], states: [] });
+
+  useEffect(() => {
+    setCachedSelection((x) => ({ ...x, [region]: selected }));
+  }, [region, selected]);
 
   const handleChange = (event, option) => {
     if (region === option.id) return;
-    useLocationStore.setState({ selected: [] });
+    useLocationStore.setState({ selected: cachedSelection[option.id] });
     option?.id && setRegion(option.id);
     // HACK: force a slight zoom so the map re-renders (otherwise, the map is blank until the user moves it)
     map?.zoomTo(map?.getZoom() - 0.001);
   };
+
+  console.log({ selected, cachedSelection });
 
   return (
     <InlineMenu
