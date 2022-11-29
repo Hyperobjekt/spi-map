c1fce316630ccbf406510c2ce6da0aefa2237abf
Chris Hull <chris.f.hull@gmail.com>
2022-11-29 14:43:13 +0000

    adds peer locations

--- src/App/Scorecards/components/ScorecardTable.js
+++ src/App/Scorecards/components/ScorecardTable.js
@@ -1,5 +1,6 @@
-import { getFormatter, useLang } from '@hyperobjekt/react-dashboard';
+import { getFormatter, useLang, useDashboardStore } from '@hyperobjekt/react-dashboard';
 import {
+  Chip,
   Icon,
   Table,
   TableBody,
@@ -18,6 +19,8 @@ import ScorecardValueCell from './ScorecardValueCell';
 import useIndicatorPanelStore from '../../IndicatorPanel/store';
 import { HelpOutline } from '@mui/icons-material';
 import styled from '@emotion/styled';
+import { GEOID_TO_PLACE_NAME } from 'App/utils';
+import { startCase } from 'lodash';
 
 const HintIcon = styled(Icon)(({ theme }) => ({
   opacity: 0.54,
@@ -31,23 +34,39 @@ const HintIcon = styled(Icon)(({ theme }) => ({
 
 export const ScorecardTable = React.forwardRef(
   ({ locations: baseLocations, metrics: baseMetrics, ...props }, ref) => {
+    const region = useDashboardStore((state) => state.region);
+
+    const [peers, setPeers] = useState([]);
     const [demographics, setDemographics] = useState([]);
-    const locations = baseLocations.map((l) => {
-      const isState = l.state && l.GEOID && l.state === l.GEOID;
-      const data = l.GEOID
-        ? demographics.find(
-            (d) =>
-              d?.geoid &&
-              (isState
-                ? d.geoid.startsWith('04000') && d.geoid.endsWith(l.GEOID)
-                : d.geoid.endsWith(l.GEOID)),
-          )
-        : null;
-      return {
-        ...l,
-        ...(data || {}),
-      };
-    });
+
+    const locations = baseLocations
+      .map((l) => {
+        const isState = l.state && l.GEOID && l.state === l.GEOID;
+        const data = l.GEOID
+          ? demographics.find(
+              (d) =>
+                d?.geoid &&
+                (isState
+                  ? d.geoid.startsWith('04000') && d.geoid.endsWith(l.GEOID)
+                  : d.geoid.endsWith(l.GEOID)),
+            )
+          : null;
+        return {
+          ...l,
+          ...(data || {}),
+        };
+      })
+      .map((l) => {
+        const { LocationGeoid, ...locationPeers } =
+          peers.find((d) => d?.LocationGeoid && d.LocationGeoid.endsWith(l.GEOID)) ?? {};
+
+        return {
+          ...l,
+          peers: Object.values(locationPeers || [])
+            .map((x) => GEOID_TO_PLACE_NAME[x])
+            .sort(),
+        };
+      });
 
     const customizedMetrics = useIndicatorPanelStore((state) => state.customizedMetrics);
     const hasCustomized = customizedMetrics.length > 0;
@@ -161,6 +180,22 @@ export const ScorecardTable = React.forwardRef(
         depth: 2,
         formatter: getFormatter('percent'),
       },
+      {
+        id: 'peers',
+        name: `Peer ${startCase(region)}`,
+        depth: 0,
+        cellStyle: { verticalAlign: 'top' },
+        formatter: (locations) => (
+          <div style={{ display: 'flex', gap: 8, flexWrap: 'wrap', fontWeight: 'normal' }}>
+            {locations.map((location, i) => (
+              <>
+                {i > 0 && <span>•</span>}
+                <span>{location}</span>
+              </>
+            ))}
+          </div>
+        ),
+      },
     ];
 
     useEffect(() => {
@@ -178,10 +213,20 @@ export const ScorecardTable = React.forwardRef(
         },
       });
 
+      Papa.parse(`/assets/data/${region}-peers.csv`, {
+        header: true,
+        download: true,
+        dynamicTyping: true,
+        skipEmptyLines: true,
+        complete: (data) => {
+          setPeers(data.data);
+        },
+      });
+
       return () => {
         subscribed = false;
       };
-    }, []);
+    }, [region]);
 
     return (
       <TableContainer ref={ref} {...props}>
@@ -227,7 +272,7 @@ const Row = ({ metric, locations }) => {
       key={metric.id}
       className={clsx('scorecard__row', 'scorecard__row--depth' + metric.depth)}
     >
-      <TableCell className="scorecard__label-cell">
+      <TableCell className="scorecard__label-cell" style={metric.cellStyle}>
         {/* SCROLL TARGET: offset by 90px to make room for header */}
         <span
           id={`scorecard-row-${metric.id}`}
@@ -294,6 +339,7 @@ const Row = ({ metric, locations }) => {
             }
             performance={performance}
             percent={showPercent && value ? value / 100 : undefined}
+            style={metric.cellStyle}
           />
         );
       })}
